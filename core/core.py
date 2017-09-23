ddae91433b191a447189b21656194380a38a208f
david <danieli.davidalexandre@gmail.com>
2017-09-23 19:06:00 +0200

    Creation of the index

--- core/core.py
+++ core/core.py
@@ -6,6 +6,7 @@ import collections
 import kea.kea as kea
 from tqdm import tqdm
 
+
 def word_count_dict(filename):
     """
     Returns a word/count DICT for this filename.
@@ -37,21 +38,77 @@ def word_count_dict(filename):
 
     return word_count
 
+def one_document_index_creation(filename):
+    """
+    Creates and return an index for this filename.
+    """
+
+    index = {}
+    input_file = open(filename, 'r')
+    stopwords_file = open('data/stopwords-fr.txt', 'r')
+    stopwords = []
+    for stopword in stopwords_file:
+        stopwords.append(stopword.split()[0])
+    stopwords = set(stopwords)
+
+    for line in input_file:
+
+        keatokenizer = kea.tokenizer()
+        words = keatokenizer.tokenize(line)
+
+        for word in words:
+            word = word.lower()
+            if word not in stopwords:
+                if not word in index:
+                    index[word] = [filename]
+
+    input_file.close()
+
+    return index
+
+def index_creation(dir):
+    """
+    Creates and return the index for all the files in the directory.
+    """
+
+    index_update = {}
+
+    for filename in tqdm(os.listdir(dir)):
+        index_temp = one_document_index_creation('{}/{}'.format(dir, filename))
+
+        for word in index_temp.keys():
+            if word in index_update:
+                index_update[word] = index_update[word] + index_temp[word]
+            else:
+                index_update[word] = index_temp[word]
+    return index_update
+
+
 def main():
     dir = 'data/lemonde-utf8'
     os.chdir('..')
+
+    #word_count
     word_count = {}
 
     for filename in tqdm(os.listdir(dir)):
         word_count_update = word_count_dict('{}/{}'.format(dir, filename))
 
-        #merge
+        # merge
         first = collections.Counter(word_count)
         second = collections.Counter(word_count_update)
         word_count = dict(first + second)
 
     word_count_list = sorted(word_count.items(), key=lambda t: t[1])
     print('{}'.format(word_count_list[-1::-21]))
+
+
+    #index
+    index=index_creation(dir)
+
+    for key,value in index.items():
+        print("{}->{}".format(key,value))
+
 
 if __name__ == '__main__':
     time0 = time.time()
