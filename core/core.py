379c6a64b7c16c2f13cf9cd66018ace3f6955981
padipadou <paul.alexis.dray@gmail.com>
2017-09-27 11:36:15 +0200

    creating data dictionary: key = file num value = file content

--- core/core.py
+++ core/core.py
@@ -6,6 +6,18 @@ import collections
 import kea.kea as kea
 from tqdm import tqdm
 
+def collect_datadoc_dict():
+    dir = 'data/lemonde-utf8'
+    os.chdir('..')
+
+    dict = {}
+    for i,filename in enumerate(os.listdir(dir)):
+        path = '{}/{}'.format(dir, filename)
+        with open(path, 'r') as infile:
+            dict[i] = infile.read()
+
+    return dict
+
 
 def word_count_dict(filename):
     """
@@ -20,19 +32,20 @@ def word_count_dict(filename):
         stopwords.append(stopword.split()[0])
     stopwords = set(stopwords)
 
+    keatokenizer = kea.tokenizer()
+
     for line in input_file:
-        #words = line.split()
+        words = line.split()
 
-        keatokenizer = kea.tokenizer()
-        words = keatokenizer.tokenize(line)
+        #words = keatokenizer.tokenize(line)
 
-        for word in words:
-            word = word.lower()
-            if word not in stopwords:
-                if not word in word_count:
-                    word_count[word] = 1
-                else:
-                    word_count[word] = word_count[word] + 1
+        # for word in words:
+        #     word = word.lower()
+        #     if word not in stopwords:
+        #         if not word in word_count:
+        #             word_count[word] = 1
+        #         else:
+        #             word_count[word] = word_count[word] + 1
 
     input_file.close()
 
@@ -74,6 +87,9 @@ def one_document_index_creation(filename,dico):
         for word in words:
             word = word.lower()
             if word not in stopwords:
+                doclist = index.get(word, )
+
+                index[word] = count + 1
                 if not word in index:
                     index[word] = [dico[filename]]
                 else:
@@ -110,42 +126,48 @@ def index_creation(dir,dico):
 
 
 def main():
-    dir = 'data/lemonde-utf8'
-    os.chdir('..')
+
+    datadict = collect_datadoc_dict()
+
+    print(datadict[1])
+
+    #dir = 'data/lemonde-utf8'
+    #os.chdir('..')
 
     #word_count
-    word_count = {}
+    #word_count = {}
 
-    for filename in tqdm(os.listdir(dir)):
-        word_count_update = word_count_dict('{}/{}'.format(dir, filename))
+    # for filename in tqdm(os.listdir(dir)):
+    #     word_count_update = word_count_dict('{}/{}'.format(dir, filename))
+    #
+    #     # merge
+    #     first = collections.Counter(word_count)
+    #     second = collections.Counter(word_count_update)
+    #     word_count = dict(first + second)
+    #
+    # word_count_list = sorted(word_count.items(), key=lambda t: t[1])
+    # print('{}'.format(word_count_list[-1::-21]))
 
-        # merge
-        first = collections.Counter(word_count)
-        second = collections.Counter(word_count_update)
-        word_count = dict(first + second)
-
-    word_count_list = sorted(word_count.items(), key=lambda t: t[1])
-    print('{}'.format(word_count_list[-1::-21]))
-
-    dico=transform_page_names_into_numbers(dir);
+    #dico=transform_page_names_into_numbers(dir);
 
     #index
-    index=index_creation(dir,dico)
-
-    print("Display of the index:")
-
-    for key,value in index.items():
-        print("{}->{}".format(key,value))
-
-    print("\nDisplay of dico:")
-    dico_list = sorted(dico.items(), key=lambda t: t[1])
-    print('{}'.format(dico_list))
+    # index=index_creation(dir,dico)
+    #
+    # print("Display of the index:")
+    #
+    # for key,value in index.items():
+    #     print("{}->{}".format(key,value))
+    #
+    # print("\nDisplay of dico:")
+    # dico_list = sorted(dico.items(), key=lambda t: t[1])
+    # print('{}'.format(dico_list))
 
 
 if __name__ == '__main__':
     time0 = time.time()
 
     main()
+
     duration = time.time() - time0
 
     print("Temps d'execution = {} seconde(s)\n".format(duration))
