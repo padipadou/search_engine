9394e3d01b177eea2abc0928618d16dd5fb91505
Alexandre Do <alexandre.do1@etu.upmc.fr>
2017-10-02 22:03:06 +0200

    adding text_to_test, core_test, load_data_test, test_index, stopwords modifications

---
+++ tests/core_test.py
@@ -0,0 +1,23 @@
+import core.index_data as id
+import load_data_test as ld
+import time
+
+def main():
+    print("Loading data...")
+    data_dict, name_num_dict, num_name_dict = ld.load_data_dict()
+    stopwords = ld.load_stopwords_set()
+
+    print("Creating index...")
+    index_dict, word_num_dict, num_word_dict = \
+        id.create_index_dict(data_dict, stopwords)
+
+    print(index_dict)
+
+if __name__ == '__main__':
+    time0 = time.time()
+
+    main()
+
+    duration = time.time() - time0
+
+    print("Temps d'execution = {} seconde(s)\n".format(duration))
