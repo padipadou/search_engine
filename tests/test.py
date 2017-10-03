07bfe96578136846838d30954e4fc10bf4d6face
Alexandre Do <alexandre.do1@etu.upmc.fr>
2017-10-03 20:38:23 +0200

    adding test_word_num_dict and test_num_word_dict removing test_index

--- tests/test.py
+++ tests/test.py
@@ -4,19 +4,29 @@ import tests.load_data_test as ld
 
 class test_search_engine(unittest.TestCase):
 
-    def test_index(self):
+    def test_word_num_dict(self):
 
-        # test OK pour majuscule, tiret, retour de ligne, saut de ligne, mots vides
-        # ne prend pas en compte les accents et les ponctuations
-        # retourne OK si 'expected' et 'index_dict' retournent
-        # la même séquence (num_mot: {num_doc: [position])
+        # test OK pour majuscule, tiret, retour de ligne, stemming, mots vides, ponctuation
+        # ne prend pas en compte les accents
+        # retourne OK si 'expected' et 'word_num_dict' correspondent
 
         data_dict, name_num_dict, num_name_dict = ld.load_data_dict()
         stopwords = ld.load_stopwords_set()
-        index_dict, word_num_dict, num_word_dict = \
-        test = id.create_index_dict(data_dict, stopwords)
-        expected = {0: {0: [1]}, 1: {0: [2, 7]}, 2: {0: [3]}, 3: {0: [4]}, 4: {0: [5]}, 5: {0: [6]}, 6: {0: [8]}}
-        self.assertEqual(index_dict, expected, msg='Test failed')
+        index_dict, word_num_dict, num_word_dict = id.create_index_dict(data_dict, stopwords)
+        expected = 14
+        self.assertEqual(word_num_dict["vernaculair"], expected, msg='Test failed')
+
+    def test_num_word_dict(self):
+
+        # test OK pour majuscule, tiret, retour de ligne, stemming, mots vides, ponctuations
+        # ne prend pas en compte les accents
+        # retourne OK si 'expected' et 'num_word_dict' correspondent
+
+        data_dict, name_num_dict, num_name_dict = ld.load_data_dict()
+        stopwords = ld.load_stopwords_set()
+        index_dict, word_num_dict, num_word_dict = id.create_index_dict(data_dict, stopwords)
+        expected = 'term'
+        self.assertEqual(num_word_dict[1], expected, msg='Test failed')
 
 if __name__ == '__main__':
     unittest.main()
