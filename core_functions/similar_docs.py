dbb1b7ce708c05e15a91f6330795c33e35692627
padipadou <paul.alexis.dray@gmail.com>
2017-10-04 22:45:35 +0200

    docs similarity finished

--- core_functions/similar_docs.py
+++ core_functions/similar_docs.py
@@ -24,7 +24,7 @@ def calculate_tf_idf_dict(index_dict):
 
 def create_doc_vector_dict(docnum, tf_idf_dict):
     """
-    Creates and returns a dict linked to a DOC with words as keys in it and the tf*idf as values for each of them.
+    Creates and returns a dict linked to only ONE doc with words as keys in it and the tf*idf as values for each of them.
     WARNING: it requires to be run after the calculation of tf*idf measures
     """
     doc_dict_final = {}
@@ -41,19 +41,27 @@ def create_doc_vector_dict(docnum, tf_idf_dict):
 
 def euclidean_norm(words_doc_dict):
     """
-
+    Returns Euclidean norm for any words_doc_dict vector.
+    WARNING: the dict must have the correct form
+        key: wordnum
+        value: tf*idf
     """
     sum = 0
 
     for wordnum in words_doc_dict:
         sum += words_doc_dict[wordnum] * words_doc_dict[wordnum]
 
-    return sqrt(sum)
+    euclidean_norm = sqrt(sum)
+
+    return euclidean_norm
 
 
-def cosine_similarity(words_doc1_dict, words_doc2_dict):
+def calculate_cosine(words_doc1_dict, words_doc2_dict):
     """
-
+    Returns cosine between two vectors, each of them are linked to one document.
+    WARNING: the dicts must have the correct form
+        key: wordnum
+        value: tf*idf
     """
     words_doc1_set = set(words_doc1_dict.keys())
     words_doc2_set = set(words_doc2_dict.keys())
@@ -73,7 +81,7 @@ def cosine_similarity(words_doc1_dict, words_doc2_dict):
 
 def calculate_docs_similarity(docname1, docname2, name_num_dict, tf_idf_dict):
     """
-
+    Full function to get similarity between two docs.
     """
     docnum1 = ld.get_docnum_from_name(docname1, name_num_dict)
     docnum2 = ld.get_docnum_from_name(docname2, name_num_dict)
@@ -81,7 +89,7 @@ def calculate_docs_similarity(docname1, docname2, name_num_dict, tf_idf_dict):
     words_doc1_dict = create_doc_vector_dict(docnum1, tf_idf_dict)
     words_doc2_dict = create_doc_vector_dict(docnum2, tf_idf_dict)
 
-    docs_similarity = cosine_similarity(words_doc1_dict, words_doc2_dict)
+    docs_similarity = calculate_cosine(words_doc1_dict, words_doc2_dict)
 
     return docs_similarity
 
