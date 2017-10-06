8979d9665ab28e7c3ef09745a0d58914869eb72b
padipadou <paul.alexis.dray@gmail.com>
2017-10-06 19:56:55 +0200

    adding new infos dict adding new class of Const

---
+++ core_functions/__init__.py
@@ -0,0 +1,11 @@
+class Const:
+    def __init__(self):
+        pass
+
+    # Variants for Term Frequency Weight
+    # binary, raw_count, term_frequency, log_frequency, double_normal_05
+    TF_WEIGHT = 'raw_count'
+
+    # Variants for Inverse Document Frequency Weight
+    # unary, idf, idf_smooth, idf_max, idf_probalistic
+    IDF_WEIGHT = ''
--- core_functions/index_data.py
+++ core_functions/index_data.py
@@ -7,6 +7,7 @@ def create_index_dict(datadict, stopwords):
     Creates and returns the positional index for all the files in datadict.
     Creates and returns a dict containing a word as key, wordnum as value for all the words in datadict.
     Creates and returns a dict containing a wordnum as key, word as value for all the words in datadict.
+    Creates and returns a dict containing a docnum as key, number of interesting words as value for all the files in datadict.
     """
     # tokenizer, from Kea
     tokenizer = kea.tokenizer()
@@ -17,10 +18,12 @@ def create_index_dict(datadict, stopwords):
     word_num_dict = {}
     num_word_dict = {}
     index_dict = {}
+    infos_doc_dict = {}
     word_count = 0
 
     for page_number in tq.tqdm(datadict.keys()):
         word_position = 0
+        term_frequency_max = 0
         content_page = datadict[page_number].lower().split('\n')
 
         for line in content_page:
@@ -41,6 +44,8 @@ def create_index_dict(datadict, stopwords):
                         num_word_dict[word_count] = wordstem
                         index_dict[word_count] = {page_number: [word_position]}
 
+                        if term_frequency_max < 1: term_frequency_max = 1
+
                         word_count += 1
 
                     # word ALREADY in the index, word NOT YET in the page
@@ -51,11 +56,20 @@ def create_index_dict(datadict, stopwords):
                     elif word_num >= 0 and page_number in index_dict[word_num]:
                         index_dict[word_num][page_number] += [word_position]
 
+                        if term_frequency_max < len(index_dict[word_num][page_number]):
+                            term_frequency_max = len(index_dict[word_num][page_number])
+
                     # ERROR
                     else:
                         raise Exception('Issue with word: "{}" \n\tin the page {} \n\tat the position {}'.format(wordstem, page_number, word_position))
 
-    return index_dict, word_num_dict, num_word_dict
+        # total nb of words
+        infos_doc_dict[page_number][0] = word_position
+
+        # term frequency max
+        infos_doc_dict[page_number][1] = term_frequency_max
+
+    return index_dict, word_num_dict, num_word_dict, infos_doc_dict
 
 if __name__ == '__main__':
     pass
