5d5e32ae6f3a1b6058e6018c67ffab293833bdd2
padipadou <paul.alexis.dray@gmail.com>
2017-10-06 20:01:24 +0200

    New ways to calculate tf and idf weights. WARNING: one way still missing in idf weight calculation

--- core_functions/similar_docs.py
+++ core_functions/similar_docs.py
@@ -1,7 +1,55 @@
 import core_functions.load_data as ld
+from core_functions import Const
 from math import sqrt, log10
 
-def calculate_tf_idf_dict(index_dict):
+
+def tf_function(positions_list, infos_doc):
+    """
+    More infos https://en.wikipedia.org/wiki/Tf%E2%80%93idf#Term_frequency_2
+    :param positions_list:
+    :param infos_doc:
+    :return:
+    """
+
+    nb_total_words = infos_doc[0]
+    term_freq_max = infos_doc[1]
+
+    if Const.TF_WEIGHT == 'binary':
+        return 1
+    elif Const.TF_WEIGHT == 'raw_count':
+        return len(positions_list)
+    elif Const.TF_WEIGHT == 'term_frequency':
+        return len(positions_list) / nb_total_words
+    elif Const.TF_WEIGHT == 'log_frequency':
+        return 1 + log10(len(positions_list))
+    elif Const.TF_WEIGHT == 'double_normal_05':
+        return 0.5 + 0.5 * (len(positions_list)/term_freq_max)
+
+
+def idf_function(word_dict, infos_doc_dict):
+    """
+    More infos https://en.wikipedia.org/wiki/Tf%E2%80%93idf#Inverse_document_frequency_2
+    :param word_dict:
+    :param infos_doc_dict:
+    :return:
+    """
+
+    N_total_docs = len(infos_doc_dict)
+    n_docs_term = len(word_dict)
+
+    if Const.IDF_WEIGHT == 'unary':
+        return 1
+    elif Const.IDF_WEIGHT == 'idf':
+        return log10(N_total_docs/n_docs_term)
+    elif Const.TF_WEIGHT == 'idf_smooth':
+        return log10(1 + N_total_docs/n_docs_term)
+    elif Const.TF_WEIGHT == 'idf_max':
+        return 0
+    elif Const.TF_WEIGHT == 'idf_probalistic':
+        return log10((N_total_docs - n_docs_term)/n_docs_term)
+
+
+def calculate_tf_idf_dict(index_dict, infos_doc_dict):
     """
     Calculates a dict of words as keys and dicts as values.
     For each dict, you have docnums as keys and tf*idf as values.
@@ -10,13 +58,14 @@ def calculate_tf_idf_dict(index_dict):
 
     for wordnum in index_dict.keys():
         word_dict = index_dict.get(wordnum)
-        # idf = 1/len(word_dict)
-        idf = log10(107/len(word_dict)) # variant from Mr Tannier's TP
+
+        idf = idf_function(word_dict, infos_doc_dict)
         tf_idf_word_dict = {}
 
         for document in word_dict.keys():
             positions_list = word_dict.get(document)
-            tf = len(positions_list)
+            tf = tf_function(positions_list, infos_doc_dict[document])
+
             tf_idf_word_dict[document] = tf * idf
 
         tf_idf_dict[wordnum] = tf_idf_word_dict
