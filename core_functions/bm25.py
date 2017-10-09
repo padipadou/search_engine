3b3d204f6a6eaa910f8eddef95e9f64cf13477eb
JF <thai.jfrancois@gmail.com>
2017-10-09 16:21:01 +0200

    okapi bm25 (ranking function for a query)

---
+++ core_functions/bm25.py
@@ -0,0 +1,26 @@
+import libs.kea as kea
+import Stemmer as pystemmer
+
+
+def bm25_function(query, stopwords):
+    # tokenizer, from Kea
+    tokenizer = kea.tokenizer()
+
+    # stemmer, from PyStemmer
+    stemmer = pystemmer.Stemmer('french')
+
+    query_list = []
+    words_query = tokenizer.tokenize(query)
+
+    for word in words_query:
+        if word not in stopwords:
+            wordstem = stemmer.stemWord(word)
+            query_list.append(wordstem)
+
+    #calcul de bm25 pour chaque document
+
+    return 1
+
+
+if __name__ == '__main__':
+    pass
