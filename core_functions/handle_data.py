a73c193b705c091d3c44b73789631d7b655ef799
padipadou <paul.alexis.dray@gmail.com>
2017-11-04 12:17:02 +0100

    handle data clean and without a little bug "daydir'"

--- core_functions/handle_data.py
+++ core_functions/handle_data.py
@@ -1,11 +1,15 @@
 import os
 
 
-def load_data_dict(directory, number_files=None, indice_start=0):
+def load_data_dict(directory, nb_files_needed=None, indice_start=0):
     """
-    Creates and returns a dict containing file num as key, file content as value.
-    Creates and returns a dict containing filename as key, filenum as value.
-    Creates and returns a dict containing filenum as key, filename as value.
+    :param directory: root to read files
+    :param nb_files_needed: number of files that you need
+    :param indice_start: first file that you need
+    :return:
+    data_dict: dict with docnum as key, content of doc as value
+    name_num_dict: dict with doc name as key, docnum as value
+    num_name_dict: dict with docnum as key, doc name  as value
     """
 
     data_dict = {}
@@ -13,8 +17,8 @@ def load_data_dict(directory, number_files=None, indice_start=0):
     num_name_dict = {}
 
     # Initial mode: with 100 docs
-    if number_files is None:
-        # dir_ = 'data/lemonde-utf8'
+    if nb_files_needed is None:
+        # dir_ = 'data/lemonde_utf8'
         for i, filename in enumerate(os.listdir(directory)):
             path = '{}/{}'.format(directory, filename)
             name_num_dict[filename] = i
@@ -33,39 +37,32 @@ def load_data_dict(directory, number_files=None, indice_start=0):
             for month_dir in os.listdir(current_path_1):
                 current_path_2 = current_path_1 + '/' + month_dir
                 for day_dir in os.listdir(current_path_2):
-                    current_path_3 = current_path_2 + '/' + month_dir
+                    current_path_3 = current_path_2 + '/' + day_dir
                     for filename in os.listdir(current_path_3):
                         file_count += 1
                         # No need to read this file
                         if file_count < indice_start:
                             pass
-                        # This file is necessary for this batch
-                        elif file_count >= indice_start and i_global < number_files:
+
+                        # This file is necessary for this batch (if it exists)
+                        elif file_count >= indice_start and i_global < nb_files_needed:
                             current_path_4 = current_path_3 + '/' + filename
                             name_num_dict[filename] = i_global
                             num_name_dict[i_global] = filename
 
-                            with open(current_path_4, 'r') as infile:
-                                data_dict[i_global] = infile.read()
-                            i_global += 1
+                            try:
+                                with open(current_path_4, 'r') as infile:
+                                    data_dict[i_global] = infile.read()
+                                i_global += 1
+                            except:
+                                return data_dict, name_num_dict, num_name_dict
+                                print("Not enough files ! Only {} here.".format(i_global))
+
                         # We have enough files for this batch
-                        elif i_global >= number_files:
+                        elif i_global >= nb_files_needed:
                             return data_dict, name_num_dict, num_name_dict
 
     return data_dict, name_num_dict, num_name_dict
-
-
-def get_docnum_from_name(name, name_num_dict):
-    """
-    Returns a num linked to a name (docname).
-    """
-    docnum = name_num_dict.get(name,-1)
-
-    if docnum >= 0:
-        return docnum
-    # ERROR
-    else:
-        raise Exception("'{}' is not a document !")
 
 
 def load_stopwords_set(path='data/stopwords-fr.txt'):
@@ -80,6 +77,10 @@ def load_stopwords_set(path='data/stopwords-fr.txt'):
 
     return set(stopwords)
 
+
+def remove_old_files_for_bloc(bloc_num):
+    os.remove("data/*_b" + str(bloc_num) + ".pickle")
+    return ''
 
 if __name__ == '__main__':
     pass
