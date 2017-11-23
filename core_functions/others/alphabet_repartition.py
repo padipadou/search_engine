ad838589fe3de0a22a9ead3d018b2f477922f652
padipadou <paul.alexis.dray@gmail.com>
2017-11-23 12:59:32 +0100

    alphabet_repartition: depth = 2

---
+++ core_functions/others/alphabet_repartition.py
@@ -0,0 +1,114 @@
+import core_functions.handle_data as hd
+import core_functions.pickle_usage as pck
+import core_functions.normalization as nrm
+import libs.kea as kea
+import tqdm as tq
+
+
+def creation_alpha_dict(depth):
+    alphabet_list = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+                     "u", "v", "w", "x", "y", "z"]
+    alpha_dict = {}
+    for first_letter in alphabet_list:
+        if depth == 1:
+            key_ = first_letter
+            alpha_dict[key_] = 0
+        elif depth == 2:
+            for second_letter in alphabet_list:
+                key_ = first_letter + second_letter
+                alpha_dict[key_] = 0
+        elif depth == 3:
+            for second_letter in alphabet_list:
+                for third_letter in alphabet_list:
+                    key_ = first_letter + second_letter + third_letter
+                    alpha_dict[key_] = 0
+
+    alpha_dict["0others"] = 0
+
+    return alpha_dict
+
+
+def repartition(size_bloc=100):
+    alpha_dict_1 = creation_alpha_dict(1)
+    alpha_dict_2 = creation_alpha_dict(2)
+
+    data_dict, name_num_dict, num_name_dict = \
+        hd.load_data_dict("../../data/text_10000", size_bloc)
+
+    del name_num_dict
+    del num_name_dict
+
+    # tokenizer, from Kea
+    tokenizer = kea.tokenizer()
+
+    stopwords = hd.load_stopwords_set('../../data/stopwords-fr.txt')
+
+    for page_number in tq.tqdm(data_dict.keys()):
+        content_page = data_dict[page_number].split('\n')
+        for line in content_page:
+            words_line = tokenizer.tokenize(line)
+            for word in words_line:
+                if word.lower() not in stopwords and len(word) > 2:
+                    norm_word = nrm.accents_removal(word)
+                    key_3 = norm_word[:3]
+                    key_2 = norm_word[:2]
+                    key_1 = norm_word[:1]
+
+                    if alpha_dict_2.get(key_2, -1) >= 0:
+                        alpha_dict_2[key_2] += 1
+                        alpha_dict_1[key_1] += 1
+                    else:
+                        alpha_dict_2["0others"] += 1
+                        alpha_dict_1["0others"] += 1
+
+    return alpha_dict_1, alpha_dict_2
+
+
+def write_csv(alpha_dict, total_word_nb):
+    f = open('../../data/aphabet_repartition.csv', 'w')
+
+    for key, value in sorted(alpha_dict.items(), key=lambda x: x[0], reverse=False):
+        row = "{};{}\n".format(key, value/total_word_nb)
+        f.write(row)
+
+    f.close()
+
+
+def repartition_calc(alpha_dict, total_word_nb, groups_nb):
+    val_nb_group = int(total_word_nb/groups_nb)
+
+    val_nb = 0
+    start_end_groups = []
+    start_key = "00"
+    for key, value in sorted(alpha_dict.items(), key=lambda x: x[0], reverse=False)[1:]:
+        val_nb += value
+        if val_nb > val_nb_group:
+            start_end_groups.append([start_key, prev_key, val_nb, val_nb / total_word_nb])
+            start_key = key
+            val_nb = value
+        prev_key = key
+    if val_nb > 0:
+        start_end_groups.append([start_key, prev_key, val_nb, val_nb/total_word_nb])
+
+    val_nb = alpha_dict["0others"]
+    start_end_groups.append(["0others", val_nb, val_nb/total_word_nb])
+
+    return start_end_groups
+
+
+if __name__ == '__main__':
+    # alpha_dict_1, alpha_dict_2 = repartition()
+    # pck.pickle_store("alpha_dict_2", alpha_dict_2, "../../")
+
+    alpha_dict_2 = pck.pickle_load("alpha_dict_2", "../../")
+    total_word_nb = 0
+    for val_ in alpha_dict_2.values():
+        total_word_nb += val_
+
+    groups_nb = 50
+    start_end_groups = repartition_calc(alpha_dict_2, total_word_nb, groups_nb)
+    for start_end_group in start_end_groups:
+        print(start_end_group)
+
+
+    # write_csv(alpha_dict, total_word_nb)
