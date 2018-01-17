8db9bc6b46b7dcc66393d78c501cce66afebbba9
padipadou <paul.alexis.dray@gmail.com>
2018-01-17 22:43:23 +0100

    size mem 500mo

--- src/__init__.py
+++ src/__init__.py
@@ -37,7 +37,7 @@ class Const:
     POSITIONS_LIST = False
 
     # size max in Mo for datablocs at the beginning
-    MEMORY_SIZE = 100
+    MEMORY_SIZE = 500
 
     # size for one data batch (number of docs)
     MINIBATCH_SIZE = 1000
