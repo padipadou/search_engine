d5b34775c82ef854b5c76a389222d23e06277c1f
padipadou <paul.alexis.dray@gmail.com>
2018-02-05 22:27:51 +0100

    make cleaner word2vec, not finshed

--- src/Word2vec/test_word2vec.py
+++ src/Word2vec/test_word2vec.py
@@ -1,45 +1,53 @@
-import gensim
 import logging
-import os
-import pickle
-import sys
-from gensim.models.phrases import Phraser
-from gensim.models import Phrases, Word2Vec
-
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
- 
-lang='fr'
-mincount=50
-#mincount=1
-size=300
-ngram_level=2
-threshold=2
-remove_accents=True
-
-#on passe en argument le mot que l'on souhaite tester avec word2vec
-word = sys.argv[1]
-
-assert ngram_level in [0, 1, 2]
-
-extra = ""
-if remove_accents:
-    extra = "_remove_accents"
-
-#Chargement du modèle
-if ngram_level > 0:
-    phraser_file = 'phraser/phraser_web{}_mincount{}_t{}_level{{}}.bin'.format(lang, mincount, threshold)
-word2vec_model_file = 'word2vec/word2vec_web{}_mincount{}_size{}{}_phrases.bin'.format(lang, mincount, size, extra)
-
-model = Word2Vec.load(word2vec_model_file)
+from gensim.models import Word2Vec
 
 
-#Divers tests    
-#test = model.wv.most_similar(positive=['Nicolas', 'Sarkozy'], negative=['Francois'])
+def word2vec_test(positive_word, negative_word=None):
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-test = model.wv.most_similar(positive=[word], negative=[], topn=100)
+    lang = 'fr'
+    mincount = 50
+    # mincount = 1
+    size = 300
+    ngram_level = 2
+    threshold = 2
+    remove_accents = True
 
-# print(model['fille'])
+    # on passe en argument le mot que l'on souhaite tester avec word2vec
+    # word = sys.argv[1]
+    word = positive_word
+
+    assert ngram_level in [0, 1, 2]
+
+    extra = ""
+    if remove_accents:
+        extra = "_remove_accents"
+
+    # Chargement du modèle
+    if ngram_level > 0:
+        phraser_file = 'phraser/phraser_web{}_mincount{}_t{}_level{{}}.bin'.format(lang, mincount, threshold)
+    word2vec_model_file = 'word2vec/word2vec_web{}_mincount{}_size{}{}_phrases.bin'.format(lang, mincount, size, extra)
+
+    model = Word2Vec.load(word2vec_model_file)
+
+    # Divers tests
+    # test = model.wv.most_similar(positive=['Nicolas', 'Sarkozy'], negative=['Francois'])
+    if not negative_word:
+        test = model.wv.most_similar(positive=[word], negative=[], topn=100)
+    else:
+        test = model.wv.most_similar(positive=[word], negative=[negative_word], topn=100)
+
+    return test
 
 
-for t in test:
-    print(t)
+def word2vec_main(positive_word, negative_word=None):
+    results = word2vec_test(word)
+
+    for result in results:
+        print(result)
+
+
+if __name__ == '__main__':
+    word = "fille"
+
+    word2vec_main(word)
--- src/Word2vec/train_word2vec.py
+++ src/Word2vec/train_word2vec.py
@@ -8,19 +8,19 @@ from gensim.models import Phrases, Word2Vec
 import unicodedata
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
- 
-lang='fr'
+
+lang = 'fr'
 
 sentence_file = 'vraiall.txt'
 workers = 25
-mincount=50
-#mincount=1
-size=300
-ngram_level=2
-threshold=2
-remove_accents=True
-rebuild=False
-build_word2vec=True
+mincount = 50
+# mincount=1
+size = 300
+ngram_level = 2
+threshold = 2
+remove_accents = True
+rebuild = False
+build_word2vec = True
 
 assert ngram_level in [0, 1, 2]
 
@@ -29,15 +29,17 @@ if remove_accents:
     extra = "_remove_accents"
 
 if ngram_level > 0:
-    phraser_output_file = 'phraser/phraser_web{}_mincount{}_t{}{}_level{{}}.bin'.format(lang, mincount, extra, threshold)
+    phraser_output_file = 'phraser/phraser_web{}_mincount{}_t{}{}_level{{}}.bin'.format(lang, mincount, extra,
+                                                                                        threshold)
 word2vec_output_file = 'word2vec/word2vec_web{}_mincount{}_size{}{}_phrases.bin'.format(lang, mincount, size, extra)
+
 
 # test = 0
 
 class SentenceIterator(object):
     def __init__(self, filename):
         self.filename = filename
- 
+
     def __iter__(self):
         # i = 0
         for line in open(os.path.join(self.filename)):
@@ -47,21 +49,22 @@ class SentenceIterator(object):
                 line = unicodedata.normalize('NFD', line).encode('ascii', 'ignore').decode()
 
             sentence = line.lower().split(' ')
-                    # if ('new' in sentence and 'york' in sentence and 'times' in sentence): 
-                    # print(sentence)
-                    # if i % 100 == 0:
-                    #     print('{} ({})'.format(i, test))
-                # i = i + 1
+            # if ('new' in sentence and 'york' in sentence and 'times' in sentence):
+            # print(sentence)
+            # if i % 100 == 0:
+            #     print('{} ({})'.format(i, test))
+            # i = i + 1
             yield sentence
             # else:
             #     break
-                
+
+
 class PhraserIterator(object):
     def __init__(self, sentence_iterator, bigram_phraser, trigram_phraser):
         self.sentence_iterator = sentence_iterator
         self.bigram_phraser = bigram_phraser
         self.trigram_phraser = trigram_phraser
- 
+
     def __iter__(self):
         if self.trigram_phraser is not None:
             for sentence in self.sentence_iterator:
@@ -74,18 +77,17 @@ class PhraserIterator(object):
                 yield sentence
 
 
-
 if __name__ == '__main__':
 
-    sentences = SentenceIterator(sentence_file) # a memory-friendly iterator
+    sentences = SentenceIterator(sentence_file)  # a memory-friendly iterator
 
     ## Unigrams only
     if ngram_level == 0:
-        #iterator = sentences
+        # iterator = sentences
         bigram_phraser = None
         trigram_phraser = None
     ## Bigrams or trigrams
-    elif ngram_level in [1,2]:
+    elif ngram_level in [1, 2]:
         bigram_file = phraser_output_file.format(1)
         if isfile(bigram_file) and not rebuild:
             print('Loading existing bigram phraser from {}'.format(bigram_file))
@@ -98,7 +100,7 @@ if __name__ == '__main__':
         bigram_tokens = bigram_phraser[sentences]
         ## Bigrams only
         if ngram_level == 1:
-            #iterator = bigram_tokens
+            # iterator = bigram_tokens
             phraser = bigram_phraser
             trigram_phraser = None
         ## Trigrams
@@ -112,14 +114,12 @@ if __name__ == '__main__':
                 trigram_transformer = Phrases(bigram_tokens, min_count=mincount, threshold=threshold)
                 trigram_phraser = Phraser(trigram_transformer)
                 trigram_phraser.save(trigram_file)
-            #iterator = trigram_phraser[bigram_phraser[sentences]]
+                # iterator = trigram_phraser[bigram_phraser[sentences]]
     else:
         raise ValueError("")
 
-
     if build_word2vec:
         phraser_iterator = PhraserIterator(sentences, bigram_phraser, trigram_phraser)
-        
+
         model = gensim.models.Word2Vec(phraser_iterator, min_count=mincount, size=size, workers=workers)
         model.save(word2vec_output_file)
-
